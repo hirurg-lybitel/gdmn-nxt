90a81cf22c71102e080ef2aff896d4ad38abe060
andreik <gs1994@gmail.com>
2022-01-06 15:24:42 +0300

    added support for professional mui component set

--- apps/gdmn-nxt-web/src/main.tsx
+++ apps/gdmn-nxt-web/src/main.tsx
@@ -7,6 +7,11 @@ import { store } from './app/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ReconciliationStatement from './app/reconciliation-statement/reconciliation-statement';
+import { registerMUI } from './mui-license';
+
+// rename mui-license.ts.sample -> mui-license.ts
+// put in bought license key
+registerMUI();
 
 ReactDOM.render(
   <Provider store={store}>
