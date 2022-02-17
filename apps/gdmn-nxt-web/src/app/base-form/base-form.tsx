aa80b17f477d4a0997f7ad076422a21e71a83060
andreik <gs1994@gmail.com>
2022-02-17 18:10:03 +0300

    wip

--- apps/gdmn-nxt-web/src/app/base-form/base-form.tsx
+++ apps/gdmn-nxt-web/src/app/base-form/base-form.tsx
@@ -1,14 +1,85 @@
+import { createTheme, Grid, responsiveFontSizes } from '@mui/material';
+import { Theme, ThemeOptions } from '@mui/material/styles/createTheme';
+import { TypographyStyle, TypographyStyleOptions } from '@mui/material/styles/createTypography';
+import { styled, ThemeProvider } from '@mui/styles';
 import { Outlet } from 'react-router-dom';
 import './base-form.module.less';
+
+type MyThemeOptions = ThemeOptions & {
+  typography: {
+    footer: TypographyStyleOptions;
+  }
+};
+
+type MyTheme = Theme & {
+  typography: {
+    footer: TypographyStyle;
+  }
+};
+
+let theme = createTheme({
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    footer: {
+      fontSize: 12
+    },
+    fontSize: 12    
+  }
+} as MyThemeOptions) as MyTheme;
+theme = responsiveFontSizes(theme) as MyTheme;
+
+const Header = styled('header')({
+  flexBasis: '0 0 128px'
+});
+
+const Footer = styled('footer')({
+  flexBasis: '0 0 24px',
+  backgroundColor: theme.palette.grey['200'],
+  borderTopColor: theme.palette.grey['50'], 
+  borderBottomColor: theme.palette.grey['800'], 
+  fontSize: theme.typography.footer.fontSize,
+  padding: '4px 8px 4px 8px'
+});
+
+const Main = styled('main')({
+  minHeight: 'calc(100% - 152px)'
+});
+
+const Wrapper = styled('section')({
+  minHeight: '100vh',
+  display: 'grid',
+  gridTemplateRows: 'auto 1fr auto'
+});
 
 /* eslint-disable-next-line */
 export interface BaseFormProps {};
 
 export function BaseForm(props: BaseFormProps) {
   return (
-    <div>
-      <Outlet />
-    </div>
+    <ThemeProvider theme={theme}>
+      <Wrapper>
+        <Header>
+          top
+        </Header>
+        <Main>
+          <Outlet />
+        </Main>
+        <Footer>
+          footer
+        </Footer>
+      </Wrapper>
+    </ThemeProvider>
   );
 };
 
--- apps/gdmn-nxt-web/src/app/er-model-domains/er-model-domains.tsx
+++ apps/gdmn-nxt-web/src/app/er-model-domains/er-model-domains.tsx
@@ -1,13 +1,11 @@
 import Alert from '@mui/material/Alert/Alert';
-import Button from '@mui/material/Button/Button';
 import Snackbar from '@mui/material/Snackbar/Snackbar';
-import { DataGridPro, GridColDef, GridRowId, GridToolbar } from '@mui/x-data-grid-pro';
+import { GridColDef, GridRowId } from '@mui/x-data-grid-pro';
 import { useMemo, useState } from 'react';
 import { useGetErModelQuery } from '../features/er-model/erModelApi';
-import RefreshIcon from '@mui/icons-material/Refresh';
 import './er-model-domains.module.less';
 import Grid from '@mui/material/Grid/Grid';
-import Box from '@mui/material/Box/Box';
+import { CustomPagination, StyledDataGrid } from '../components/styled-data-grid/styled-data-grid';
 
 /* eslint-disable-next-line */
 export interface ErModelDomainsProps {}
@@ -43,53 +41,49 @@ export function ErModelDomains(props: ErModelDomainsProps) {
     { 
       field: 'entityName', 
       headerName: 'Сущность', 
-      width: 150
+      width: 200
     }
   ];
   
   return (
-    <>
-      <Box>
-        <Button onClick={ refetch } disabled={isFetching} startIcon={<RefreshIcon/>}>Обновить</Button>
-      </Box>
-      <Grid container height={800} columnSpacing={2}>
-        <Grid item xs={9}>
-          <DataGridPro
-            rows={rows}
-            columns={columns}
-            pagination
-            disableMultipleSelection
-            loading={isFetching}
-            getRowId={row => row.name}
-            onSelectionModelChange={setSelectionModel}
-            selectionModel={selectionModel}          
-            components={{
-              Toolbar: GridToolbar,
-            }}
-          />
-        </Grid>
-        <Grid item xs={3}>
-          {
-            data && selectionModel.length ?
-              <pre>
-                {
-                  JSON.stringify(data.domains[selectionModel[0]], undefined, 2)
-                }
-              </pre>
-            :
-              undefined  
-          }
-        </Grid>
+    <Grid container height="100%" columnSpacing={2}>
+      <Grid item xs={9}>
+        <StyledDataGrid
+          rows={rows}
+          columns={columns}
+          pagination
+          disableMultipleSelection
+          loading={isFetching}
+          getRowId={row => row.name}
+          onSelectionModelChange={setSelectionModel}
+          selectionModel={selectionModel} 
+          rowHeight={24}         
+          components={{
+            Pagination: CustomPagination
+          }}
+        />
+      </Grid>
+      <Grid item xs={3}>
         {
-          errorMessage &&
-          <Grid item xs={12}>
-            <Snackbar open autoHideDuration={5000}>
-              <Alert variant="filled" severity="error">{errorMessage}</Alert>
-            </Snackbar>
-          </Grid>
+          data && selectionModel.length ?
+            <pre>
+              {
+                JSON.stringify(data.domains[selectionModel[0]], undefined, 2)
+              }
+            </pre>
+          :
+            undefined  
         }
       </Grid>
-    </>
+      {
+        errorMessage &&
+        <Grid item xs={12}>
+          <Snackbar open autoHideDuration={5000}>
+            <Alert variant="filled" severity="error">{errorMessage}</Alert>
+          </Snackbar>
+        </Grid>
+      }
+    </Grid>
   );
 }
 
