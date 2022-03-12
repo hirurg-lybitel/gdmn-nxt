1eae9941fe924aedfa912167b5680bd23869fd48
hirurg-lybitel <yurashoihet@gmail.com>
2022-03-12 03:47:24 +0300

    wip

---
+++ apps/gdmn-nxt-web/src/app/layouts/MainLayout/Header/notification/notification.tsx
@@ -0,0 +1,24 @@
+import './notification.module.less';
+import { Avatar, Badge, Box, ButtonBase } from '@mui/material';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
+/* eslint-disable-next-line */
+export interface NotificationProps {}
+
+export function Notification(props: NotificationProps) {
+  return (
+    <Box
+      sx={{
+        mx: 2
+      }}
+    >
+      <ButtonBase>
+        <Badge color="error" variant="dot">
+          <NotificationsIcon />
+        </Badge>
+      </ButtonBase>
+    </Box>
+  );
+};
+
+export default Notification;
