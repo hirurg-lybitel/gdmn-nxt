6089deafdad3f716de34603d82fbfaca9c271ee2
AntanovichV <78271090+AntanovichV@users.noreply.github.com>
2022-03-16 13:37:44 +0300

    fix: change er-utils, er-types after reset commit

--- apps/gdmn-nxt-server/src/app/er/er-utils.ts
+++ apps/gdmn-nxt-server/src/app/er/er-utils.ts
@@ -1,21 +1,18 @@
-import { Expression, Entity, IEntities, IERModel, Operand, IDomains, Domain, IDomainBase, Attr, IAttrBase, IEntitySetAttr, IEntityAttr, ICrossAttrAdapter, isSeqAttr } from '@gsbelarus/util-api-types';
-import { getReadTransaction, releaseReadTransaction } from '../utils/db-connection';
-import { IAtRelation, IGedeminDocType } from './at-types';
-import { loadAtFields, loadAtRelationFields, loadAtRelations, loadGdDocumentType, adjustRelationName } from './at-utils';
-// import gdbaseRaw from './gdbase.json';
-import entitiesRaw from './entities.json';
-import { loadRDBFields } from './rdb-utils';
+import { Expression, Entity, IEntities, IERModel, Operand, IDomains, Domain, IDomainBase, Attr, IAttrBase, IEntitySetAttr, IEntityAttr, ICrossAttrAdapter, isSeqAttr } from "@gsbelarus/util-api-types";
+import { getReadTransaction, releaseReadTransaction } from "../utils/db-connection";
+import { IAtRelation, IGedeminDocType } from "./at-types";
+import { loadAtFields, loadAtRelationFields, loadAtRelations, loadGdDocumentType, adjustRelationName } from "./at-utils";
+//import gdbaseRaw from "./gdbase.json";
+import entitiesRaw from "./entities.json";
+import { loadRDBFields } from "./rdb-utils";
 
 /*
-
 We import er model of the existing Gedemin database in following order:
-
 1. Import built-in entities from gdbase.json file.
 2. Import simple entities for USR$ tables from the database.
 3. Import user defined document entities from the database.
 4. Import domains.
 5. Import attributes for entities which has been imported at steps 1-3.
-
 */
 
 const adjustValidation = (s: string | null) => {
@@ -25,15 +22,16 @@ const adjustValidation = (s: string | null) => {
     if (res) {
       return res[1];
     }
-  }
+  } 
 
   return undefined;
 };
 
 const str2cond = (s: string): (Expression | undefined) => {
+
   const extractOperand = (s: string): (Operand | Expression) => {
     const expPlus = /(\w+)\s*\+\s*((-{0,1}\d+)|('(.*)'))/ig;
-    const res = expPlus.exec(s);
+    let res = expPlus.exec(s);
 
     if (res) {
       return {
@@ -47,7 +45,7 @@ const str2cond = (s: string): (Expression | undefined) => {
           type: 'VALUE',
           value: res[3] ? Number(res[3]) : res[5]
         }
-      };
+      }
     } else {
       return {
         type: 'FIELD',
@@ -121,13 +119,13 @@ const str2cond = (s: string): (Expression | undefined) => {
         }
       };
     } else {
-      const arr = res[3].split(',').map(i => i.trim());
+      const arr = res[3].split(',').map( i => i.trim() );
       let values;
 
-      if (arr[0].charAt(0) === '\'') {
-        values = arr.map(i => i.slice(1, i.length - 1));
+      if (arr[0].charAt(0) === "'") {
+        values = arr.map( i => i.slice(1, i.length - 1) );
       } else {
-        values = arr.map(i => Number(i));
+        values = arr.map( i => Number(i) );
       };
 
       return {
@@ -226,7 +224,7 @@ interface IgdbaseImport {
  * @returns
  */
 const getEntityScore = (e: Entity, refTable: string) => {
-  const joinScore = e.adapter?.join?.reverse().findIndex(j => j.name === refTable);
+  const joinScore = e.adapter?.join?.reverse().findIndex( j => j.name === refTable );
 
   if (joinScore >= 0) {
     return 1000 - joinScore;
@@ -286,41 +284,32 @@ export const importModels = async () => {
     from the predefined json file. This format was used in
     previous GDMN platform. We converted it into current format
     and stored as entities.json.
-
     const relation2class = {} as { [relation: string]: string };
     const gdbase = gdbaseRaw as IgdbaseImport;
-
     const importGdbase = (g: IgdbaseImport, depth = 0, parent?: Entity) => {
       const rdbRelation = g.listTable ? rdbRelations[g.listTable.name] : undefined;
       const atRelation = rdbRelation ? atRelations[rdbRelation.RDB$RELATION_NAME] : undefined;
-
       let adapter: IEntityAdapter;
-
       if (g.listTable) {
         adapter = {
           name: g.listTable.name,
           alias: 'z'
         };
-
         if (parent?.adapter?.join) {
           adapter.join = [...parent.adapter.join];
         }
-
         if (g.distinctRelation) {
           relation2class[g.distinctRelation.name] = g.className;
-
           const j = {
             type: 'INNER',
             name: g.distinctRelation.name,
             alias: `z${depth}`
           } as IJoinAdapter;
-
           if (adapter.join) {
             adapter.join.push(j);
           } else {
             adapter.join = [j];
           }
-
           //ugly hack
           if (g.distinctRelation.name === 'GD_COMPANY') {
             adapter.join.push({
@@ -332,12 +321,10 @@ export const importModels = async () => {
         } else {
           relation2class[g.listTable.name] = g.className;
         }
-
         if (g.restrictCondition) {
           adapter.condition = str2cond(g.restrictCondition);
         }
       };
-
       const e: Entity = {
         type: g.listTable?.name === 'GD_DOCUMENT' ? 'DOCUMENT' : 'SIMPLE',
         parent: parent?.name,
@@ -347,22 +334,18 @@ export const importModels = async () => {
         semCategory: atRelation?.SEMCATEGORY ?? undefined,
         adapter
       };
-
       entities[e.name] = e;
-
       if (g.children) {
         for (const ch of g.children) {
           importGdbase(ch, g.abstract ? 1 : (depth + 1), e);
         }
       }
     };
-
     importGdbase(gdbase);
-
     writeFileSync('c:/temp/entities.json', JSON.stringify(entities, undefined, 2));
     */
 
-    const usrRelations = Object.values(atRelations).filter(r => r.RELATIONNAME.startsWith('USR$'));
+    const usrRelations = Object.values(atRelations).filter( r => r.RELATIONNAME.startsWith('USR$') );
 
     for (const usrRelation of usrRelations) {
       const arf = atRelationFields[usrRelation.RELATIONNAME];
@@ -372,13 +355,13 @@ export const importModels = async () => {
         continue;
       }
 
-      if (arf.find(f => f.FIELDNAME === 'ID')) {
-        const parent = arf.find(f => f.FIELDNAME === 'LB') ?
+      if (arf.find( f => f.FIELDNAME === 'ID')) {
+        const parent = arf.find( f => f.FIELDNAME === 'LB') ?
           entities['TgdcAttrUserDefinedLBRBTree']
-          : arf.find(f => f.FIELDNAME === 'PARENT') ?
-            entities['TgdcAttrUserDefinedTree']
-            :
-            entities['TgdcAttrUserDefined'];
+          : arf.find( f => f.FIELDNAME === 'PARENT') ?
+          entities['TgdcAttrUserDefinedTree']
+          :
+          entities['TgdcAttrUserDefined'];
 
         if (parent) {
           // make name looks the same way as in the Gedemin
@@ -449,6 +432,7 @@ export const importModels = async () => {
     };
 
 
+
     for (const documentType of gdDocumentType) {
       if (documentType.DOCUMENTTYPE === 'D' && documentType.HEADERRELNAME) {
         createDocEntity(dtMap[documentType.CLASSNAME || 'TgdcDocumentType'], documentType);
@@ -486,29 +470,30 @@ export const importModels = async () => {
         }
 
         if (!entityName) {
-          const found = Object.values(entities).filter(e =>
+          const found = Object.values(entities).filter( e =>
             // can't restrict on INNER joins only because of presence
             // of references to GD_COMPANYCODE relation
             // which is weak relation for TgdcCompany class
-            e.adapter?.join?.find(j => j.name === relation)
+            e.adapter?.join?.find( j => j.name === relation )
             ||
             e.adapter?.name === relation
           );
 
           if (found.length === 1) {
             entityName = found[0].name;
-          } else if (found.length) {
+          }
+          else if (found.length) {
             const sorted = found
-              .map(e => ({ e, score: getEntityScore(e, relation) }))
-              .sort((a, b) => b.score - a.score);
+              .map( e => ({ e, score: getEntityScore(e, relation) }) )
+              .sort( (a, b) => b.score - a.score );
 
             if (sorted[0].score > sorted[1].score) {
               entityName = sorted[0].e.name;
             } else {
               const getEntityDepth = (e: Entity) => e.parent ? (getEntityDepth(entities[e.parent]) + 1) : 0;
               const top = sorted
-                .filter(t => t.score === sorted[0].score)
-                .sort((a, b) => getEntityDepth(b.e) - getEntityDepth(a.e));
+                .filter( t => t.score === sorted[0].score )
+                .sort( (a, b) => getEntityDepth(b.e) - getEntityDepth(a.e) );
               entityName = top[0].e.name;
             }
           }
@@ -584,110 +569,110 @@ export const importModels = async () => {
             };
           } else {
             switch (rdbField.RDB$FIELD_TYPE) {
-            case 8:
-              domain = {
-                ...domainBase,
-                type: 'INTEGER',
-                max: 2_147_483_647,
-                min: -2_147_483_648,
-                default: extractNumDef(rdbField.RDB$DEFAULT_SOURCE)
-              };
-              break;
+              case 8:
+                domain = {
+                  ...domainBase,
+                  type: 'INTEGER',
+                  max: 2_147_483_647,
+                  min: -2_147_483_648,
+                  default: extractNumDef(rdbField.RDB$DEFAULT_SOURCE)
+                };
+                break;
 
-            case 7:
-              domain = {
-                ...domainBase,
-                type: 'INTEGER',
-                max: 32_767,
-                min: -32_768,
-                default: extractNumDef(rdbField.RDB$DEFAULT_SOURCE)
-              };
-              break;
+              case 7:
+                domain = {
+                  ...domainBase,
+                  type: 'INTEGER',
+                  max: 32_767,
+                  min: -32_768,
+                  default: extractNumDef(rdbField.RDB$DEFAULT_SOURCE)
+                };
+                break;
 
-            case 10:
-              domain = {
-                ...domainBase,
-                type: 'DOUBLE',
-                max: Number.MAX_VALUE,
-                min: Number.MIN_VALUE,
-                default: extractNumDef(rdbField.RDB$DEFAULT_SOURCE)
-              };
-              break;
+              case 10:
+                domain = {
+                  ...domainBase,
+                  type: 'DOUBLE',
+                  max: Number.MAX_VALUE,
+                  min: Number.MIN_VALUE,
+                  default: extractNumDef(rdbField.RDB$DEFAULT_SOURCE)
+                };
+                break;
 
-            case 12:
-              domain = {
-                ...domainBase,
-                type: 'DATE',
-                default: rdbField.RDB$DEFAULT_SOURCE ?? undefined
-              };
-              break;
+              case 12:
+                domain = {
+                  ...domainBase,
+                  type: 'DATE',
+                  default: rdbField.RDB$DEFAULT_SOURCE ?? undefined
+                };
+                break;
 
-            case 13:
-              domain = {
-                ...domainBase,
-                type: 'TIME',
-                default: rdbField.RDB$DEFAULT_SOURCE ?? undefined
-              };
-              break;
+              case 13:
+                domain = {
+                  ...domainBase,
+                  type: 'TIME',
+                  default: rdbField.RDB$DEFAULT_SOURCE ?? undefined
+                };
+                break;
 
-            case 14:
-            case 37:
-              domain = {
-                ...domainBase,
-                type: 'STRING',
-                maxLen: rdbField.RDB$CHARACTER_LENGTH ?? rdbField.RDB$FIELD_LENGTH,
-                charSetId: rdbField.RDB$CHARACTER_SET_ID,
-                default: rdbField.RDB$DEFAULT_SOURCE ?? undefined
-              };
-              break;
+              case 14:
+              case 37:
+                domain = {
+                  ...domainBase,
+                  type: 'STRING',
+                  maxLen: rdbField.RDB$CHARACTER_LENGTH ?? rdbField.RDB$FIELD_LENGTH,
+                  charSetId: rdbField.RDB$CHARACTER_SET_ID,
+                  default: rdbField.RDB$DEFAULT_SOURCE ?? undefined
+                };
+                break;
 
-            case 16:
-              domain = {
-                ...domainBase,
-                type: 'BIGINT',
-                max: Number.MAX_SAFE_INTEGER, // TODO: need changing to BigInt class
-                min: Number.MIN_SAFE_INTEGER,
-                default: extractNumDef(rdbField.RDB$DEFAULT_SOURCE)
-              };
-              break;
+              case 16:
+                domain = {
+                  ...domainBase,
+                  type: 'BIGINT',
+                  max: Number.MAX_SAFE_INTEGER, //TODO: need changing to BigInt class
+                  min: Number.MIN_SAFE_INTEGER,
+                  default: extractNumDef(rdbField.RDB$DEFAULT_SOURCE)
+                };
+                break;
 
-            case 27:
-              domain = {
-                ...domainBase,
-                type: 'DOUBLE',
-                max: Number.MAX_VALUE,
-                min: Number.MIN_VALUE,
-                default: extractNumDef(rdbField.RDB$DEFAULT_SOURCE)
-              };
-              break;
+              case 27:
+                domain = {
+                  ...domainBase,
+                  type: 'DOUBLE',
+                  max: Number.MAX_VALUE,
+                  min: Number.MIN_VALUE,
+                  default: extractNumDef(rdbField.RDB$DEFAULT_SOURCE)
+                };
+                break;
 
-            case 35:
-              domain = {
-                ...domainBase,
-                type: 'TIMESTAMP',
-                default: rdbField.RDB$DEFAULT_SOURCE ?? undefined
-              };
-              break;
+              case 35:
+                domain = {
+                  ...domainBase,
+                  type: 'TIMESTAMP',
+                  default: rdbField.RDB$DEFAULT_SOURCE ?? undefined
+                };
+                break;
 
-            case 23:
-              domain = {
-                ...domainBase,
-                type: 'BOOLEAN',
-                default: extractBooleanDef(rdbField.RDB$DEFAULT_SOURCE) ?? undefined
-              };
-              break;
+              case 23:
+                domain = {
+                  ...domainBase,
+                  type: 'BOOLEAN',
+                  default: extractBooleanDef(rdbField.RDB$DEFAULT_SOURCE) ?? undefined
+                };
+                break;
 
-              // TODO: treat text BLOBs as strings?
-            case 261:
-              domain = {
-                ...domainBase,
-                type: 'BLOB',
-                subType: rdbField.RDB$FIELD_SUB_TYPE
-              };
-              break;
+              //TODO: treat text BLOBs as strings?
+              case 261:
+                domain = {
+                  ...domainBase,
+                  type: 'BLOB',
+                  subType: rdbField.RDB$FIELD_SUB_TYPE
+                };
+                break;
 
-            default:
-              console.warn(`Unknown field type ${rdbField.RDB$FIELD_TYPE} for domain ${FIELDNAME}`);
+              default:
+                console.warn(`Unknown field type ${rdbField.RDB$FIELD_TYPE} for domain ${FIELDNAME}`);
             }
           }
 
@@ -709,16 +694,16 @@ export const importModels = async () => {
       }
 
       const candidates = entitiesArr
-        .map<[number, string] | undefined>(({ name, adapter }) => {
+        .map<[number, string] | undefined>( ({ name, adapter }) => {
           if (!adapter) {
             return undefined;
           }
 
           if (adapter.name === r) {
-            return [0, name];
+            return [0, name]
           }
 
-          const idx = adapter.join?.reverse().findIndex(j => j.name === r);
+          const idx = adapter.join?.reverse().findIndex( j => j.name === r );
 
           if (idx >= 0) {
             return [1000 - idx, name];
@@ -727,10 +712,10 @@ export const importModels = async () => {
           return undefined;
         })
         .filter(Boolean)
-        .sort((a, b) => b[0] - a[0]);
+        .sort( (a, b) => b[0] - a[0] );
 
       if (candidates.length) {
-        // console.log(`${r} ----> ${JSON.stringify(candidates)}`)
+        //console.log(`${r} ----> ${JSON.stringify(candidates)}`)
         return relation2entityNameCache[r] = candidates[0][1];
       }
 
@@ -791,8 +776,8 @@ export const importModels = async () => {
           const refEntityName = fld.GDCLASSNAME
             ? (fld.GDCLASSNAME + adjustRelationName(fld.GDSUBTYPE))
             : domain.type === 'ENTITY' || domain.type === 'ENTITY[]'
-              ? domain.entityName
-              : relation2entityName(fld.REF);
+            ? domain.entityName
+            : relation2entityName(fld.REF);
           const refEntity = refEntityName && entities[refEntityName];
 
           if (!refEntity) {
@@ -834,7 +819,7 @@ export const importModels = async () => {
       size of erModel: ${(new TextEncoder().encode(JSON.stringify(erModel, undefined, 2))).length} bytes
       domains: ${Object.keys(domains).length}
       entities: ${Object.keys(entities).length}
-      attributes: ${Object.values(entities).flatMap(e => e.attributes).length}
+      attributes: ${Object.values(entities).flatMap( e => e.attributes ).length}
     `);
 
     const stripAdapter = (attr: Attr) => {
@@ -856,7 +841,7 @@ export const importModels = async () => {
         Object.entries(erModel.entities).map(
           ([name, { adapter, attributes, ...rest }]) => ([name, {
             ...rest,
-            attributes: attributes.map(stripAdapter),
+            attributes: attributes.map( stripAdapter ),
           }])
         )
       ),
@@ -873,7 +858,7 @@ export const importModels = async () => {
         gdDocumentType
       },
       erModel,
-      /** erModel stripped of adapters as they are not needed on the client */
+      /** erModel stripped of adapters as they are not needed on the client */ 
       erModelNoAdapters
     };
   } finally {
