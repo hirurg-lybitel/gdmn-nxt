f36bc8c6c41731f4b8ab035700427e8b1146be03
hirurg-lybitel <yurashoihet@gmail.com>
2023-01-30 16:15:27 +0300

    feat: error localization

--- apps/gdmn-nxt-server/src/app/responseMessages.ts
+++ apps/gdmn-nxt-server/src/app/responseMessages.ts
@@ -1,5 +1,12 @@
 import { IResultError } from '@gsbelarus/util-api-types';
 
+const localizations = {
+  'violation of FOREIGN KEY': 'Запись нельзя удалить, так как она используется в другом документе'
+};
+
 export const resultError = (message: string):IResultError => {
+  for (const [key, value] of Object.entries(localizations)) {
+    if (message.includes(key)) return { errorMessage: value };
+  }
   return { errorMessage: message };
 };
