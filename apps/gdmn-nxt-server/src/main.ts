bb7a79d3eb7493f7d90ee1e0fd373aed032cbba2
sashazrnin <sashazernin1@gmail.com>
2023-02-22 23:19:28 +0300

    fix: Missing CSRF

--- apps/gdmn-nxt-server/src/main.ts
+++ apps/gdmn-nxt-server/src/main.ts
@@ -71,7 +71,6 @@ const path = require('path');
 app.use(express.static(path.resolve(__dirname, '../gdmn-nxt-web')));
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 const apiRoot = {
   v1: '/api/v1',
   v2: '/api/v2'
@@ -189,6 +188,7 @@ const middlewares = [
       maxAge: 24 * 60 * 60 * 1000
     }
   }),
+  cookieParser(),
   passport.initialize(),
   passport.session()
 ];
