916ede0c092e5d8ae5e098b2b1f7e884c79e828d
hirurg-lybitel <yurashoihet@gmail.com>
2023-02-24 18:24:47 +0300

    style: filters indicator
    https://github.com/gsbelarus/gdmn-nxt/issues/58

--- apps/gdmn-nxt-web/src/app/customers/dataField/DataField.tsx
+++ apps/gdmn-nxt-web/src/app/customers/dataField/DataField.tsx
@@ -1,7 +1,8 @@
-import { Chip, Box } from '@mui/material';
+import { Chip, Box, Stack } from '@mui/material';
 import style from './datafield.module.less';
 import { Theme } from '@mui/material/styles';
 import { makeStyles } from '@mui/styles';
+import { ColorMode } from '@gsbelarus/util-api-types';
 
 interface DataFieldProps {
     name: string;
@@ -10,25 +11,59 @@ interface DataFieldProps {
     deleteField: any;
 }
 
-const useStyles = makeStyles((theme: Theme) => ({
-  selectedDataContainer: {
-    position: 'relative',
-    display: 'flex',
-    alignItems: 'center',
-    padding: '15px',
-    borderRadius: '10px',
-    background: 'rgba(0,0,0,0.08)'
-  },
-  selectedDataContainer_name: {
-    padding: '0 5px',
-    top: '-12px',
-    left: '7px',
-    position: 'absolute',
-  },
-  selectedDataContainer_item: {
-    marginRight: '5px',
-  }
-}));
+const useStyles = makeStyles((theme: Theme) => {
+  const border = `1px solid rgba(189, 200, 240, ${theme.palette.mode === ColorMode.Dark ? '0.2' : '0.5'})`;
+  return {
+    mainContainer: {
+      display: 'flex',
+      flexDirection: 'column',
+      maxWidth: '100%',
+      borderLeft: border,
+      borderBottom: border,
+      borderRight: border,
+      borderRadius: theme.mainContent.borderRadius,
+      height: 'fit-content',
+      ...(theme.palette.mode === ColorMode.Dark && { background: 'rgba(0, 0, 0, 0.08)' })
+    },
+    header: {
+      display: 'flex',
+      flexDirection: 'row',
+      width: '100% !important',
+    },
+    headerTitle: {
+      display: 'flex',
+      flexDirection: 'row',
+      flexWrap: 'nowrap',
+      alignItems: 'center',
+      gap: '0.25em',
+      width: 'fit-content',
+      height: '2em',
+      margin: '-1em 0.5em 0em 0.5em',
+      overflow: 'hidden',
+      textOverflow: 'ellipsis',
+      fontSize: '1em',
+    },
+    headerBorderBefore: {
+      borderTop: border,
+      borderTopLeftRadius: theme.mainContent.borderRadius,
+      width: '1em',
+    },
+    headerBorderAfter: {
+      borderTop: border,
+      borderTopRightRadius: theme.mainContent.borderRadius,
+      width: '1em',
+      flexGrow: 2,
+    },
+    childrenContainer: {
+      display: 'flex',
+      padding: '1em',
+      paddingTop: 0,
+    },
+    childrenItem: {
+      marginRight: '5px',
+    }
+  };
+});
 
 const DataField = ({ name, data, masName, deleteField }: DataFieldProps) => {
   const onHandleDelete = (item:string) => () => {
@@ -38,13 +73,17 @@ const DataField = ({ name, data, masName, deleteField }: DataFieldProps) => {
   const classes = useStyles();
 
   return (
-    <Box style={{ margin: '0px 20px 20px 0px' }}>
-      <div className={classes.selectedDataContainer}>
-        <div className={classes.selectedDataContainer_name}>
+    <div className={classes.mainContainer}>
+      <div className={classes.header}>
+        <div className={classes.headerBorderBefore} />
+        <div className={classes.headerTitle}>
           <span>{name}</span>
         </div>
+        <div className={classes.headerBorderAfter} />
+      </div>
+      <div className={classes.childrenContainer}>
         {data.map((item: any) => (
-          <div className={classes.selectedDataContainer_item} key={item.ID}>
+          <div className={classes.childrenItem} key={item.ID}>
             <Chip
               style={{ maxWidth: '220px', margin: '0', display: 'flex' }}
               label={item.NAME || item.USR$NUMBER || item.USR$NAME}
@@ -55,7 +94,7 @@ const DataField = ({ name, data, masName, deleteField }: DataFieldProps) => {
           </div>
         ))}
       </div>
-    </Box>
+    </div>
   );
 };
 
