d410fbbc0d20311c6fd5147cd2a4e121fabf1e5a
hirurg-lybitel <yurashoihet@gmail.com>
2023-04-07 19:41:24 +0300

    chore: close window for all dialogs by one rule but with opportunity change the rule locally

--- apps/gdmn-nxt-web/src/app/customers/customers.tsx
+++ apps/gdmn-nxt-web/src/app/customers/customers.tsx
@@ -401,10 +401,7 @@ export function Customers(props: CustomersProps) {
       setFilterModel({ items: filterModels });
       setFilteringData(newValue);
     },
-    handleFilterClose: async (
-      event: any,
-      reason: 'backdropClick' | 'escapeKeyDown'
-    ) => {
+    handleFilterClose: async (event: any) => {
       if (
         event?.type === 'keydown' &&
         (event?.key === 'Tab' || event?.key === 'Shift')
