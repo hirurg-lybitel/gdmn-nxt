7825ffccf68be58e046cd9dcebc4c2756e1fad47
hirurg-lybitel <yurashoihet@gmail.com>
2023-06-07 12:56:52 +0300

    chore: fixed all errors
    https://github.com/gsbelarus/gdmn-nxt/issues/255

--- apps/gdmn-nxt-web/src/app/pages/Managment/tasksCatalogs/task-types/task-types.tsx
+++ apps/gdmn-nxt-web/src/app/pages/Managment/tasksCatalogs/task-types/task-types.tsx
@@ -1,138 +1,98 @@
 import CustomizedCard from 'apps/gdmn-nxt-web/src/app/components/Styled/customized-card/customized-card';
 import styles from './task-types.module.less';
-import { Button, CardContent, CardHeader, Stack, TextField, Typography } from '@mui/material';
+import { Button, CardContent, CardHeader, Divider, TextField, Tooltip, TooltipProps, Typography, styled, tooltipClasses } from '@mui/material';
 import StyledGrid from 'apps/gdmn-nxt-web/src/app/components/Styled/styled-grid/styled-grid';
-import { GridActionsCellItem, GridColumns, GridRowParams, GridToolbarContainer, MuiEvent, useGridApiContext, useGridApiRef } from '@mui/x-data-grid-pro';
+import { GridActionsCellItem, GridCellParams, GridColumns, GridPreProcessEditCellProps, GridRenderCellParams, GridRenderEditCellParams, GridRowParams, MuiEvent, useGridApiContext, useGridApiRef } from '@mui/x-data-grid-pro';
 import EditIcon from '@mui/icons-material/Edit';
 import SaveIcon from '@mui/icons-material/Save';
 import CancelIcon from '@mui/icons-material/Close';
 import DeleteIcon from '@mui/icons-material/Delete';
-import { ChangeEvent, useCallback, useMemo, useState } from 'react';
+import { ChangeEvent, KeyboardEvent, useCallback, useMemo, useState, MouseEvent, SyntheticEvent } from 'react';
 import { useAddTaskTypeMutation, useDeleteTaskTypeMutation, useGetTaskTypesQuery, useUpdateTaskTypeMutation } from 'apps/gdmn-nxt-web/src/app/features/kanban/kanbanCatalogsApi';
 import { ITaskType } from '@gsbelarus/util-api-types';
 import ConfirmDialog from 'apps/gdmn-nxt-web/src/app/confirm-dialog/confirm-dialog';
+import CardToolbar from 'apps/gdmn-nxt-web/src/app/components/Styled/card-toolbar/card-toolbar';
 
 /* eslint-disable-next-line */
 export interface TaskTypesProps {}
 
-function DetailPanelContent({ row: rowProp }: { row: ITaskType }) {
-  const apiRef = useGridApiContext();
-
-  // id: GridRowId;
-  // /**
-  //  * The field to put focus.
-  //  */
-  // fieldToFocus?: string;
-  // /**
-  //  * If `true`, the value in `fieldToFocus` will be deleted before entering the edit mode.
-  //  */
-  // deleteValue?: boolean;
-  // /**
-  //  * The initial value for the given `fieldToFocus`.
-  //  * If `deleteValue` is also true, this value is not used.
-  //  */
-  // initialValue?: string;
-
-
-  // id: GridRowId;
-  // /**
-  //  * Whether or not to ignore the modifications made on this cell.
-  //  * @default false
-  //  */
-  // ignoreModifications?: boolean;
-  // /**
-  //  * The field that has focus when the editing is stopped.
-  //  * Used to calculate which cell to move the focus to after finishing editing.
-  //  */
-  // field?: string;
-  // /**
-  //  * To which cell to move focus after finishing editing.
-  //  * Only works if the field is also specified, otherwise focus stay in the same cell.
-  //  * @default "none"
-  //  */
-  // cellToFocusAfter?: 'none' | 'below' | 'right' | 'left';
-
-  const updateRow = useCallback((e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    // rowProp.NAME = e.target.value ;
-    console.log('apiRef.current', apiRef.current.getRow(rowProp.ID));
-    apiRef.current.startRowEditMode({
-      id: rowProp.ID,
-      fieldToFocus: 'NAME'
-    });
-    apiRef.current.updateRows([
-      { ...rowProp, NAME: e.target.value },
-    ]);
-    apiRef.current.stopRowEditMode({
-      id: rowProp.ID,
-    });
-  }, [apiRef, rowProp]);
-
-  const initState = apiRef.current.state;
-
-  // updateRow();
-
-
-  // apiRef.current.restoreState(initState);
-
-  console.log('row', rowProp);
-
-  // const formik = useFormik<ITaskType>({
-  //   enableReinitialize: true,
-  //   validateOnBlur: false,
-  //   initialValues: {
-  //     ...rowProp
-  //   },
-  //   validationSchema: yup.object().shape({
-  //     NAME: yup
-  //       .string()
-  //       .required('')
-  //       .max(40, 'Слишком длинное наименование'),
-  //   }),
-  //   onSubmit: (value) => {
-  //     // if (!confirmOpen) {
-  //     //   setConfirmOpen(true);
-  //     //   return;
-  //     // };
-  //     // setConfirmOpen(false);
-  //   }
-  // });
-
-  return (
-    <Stack direction="row" spacing={2}>
-      {/* <FormikProvider value={formik}>
-        <Form id="taskTypes" onSubmit={formik.handleSubmit}> */}
-      <TextField
-        style={{ flex: 0.5 }}
-        // name="NAME"
-        // value={formik.values.NAME}
-        // onChange={formik.handleChange}
-        value={rowProp.NAME}
-        onChange={updateRow}
-        label="Наименование"
-      />
-      <TextField
-        value={rowProp.DESCRIPTION}
-        name="DESCRIPTION"
-        style={{ flex: 1 }}
-        // onChange={() => apiRef.current.restoreState(initState)}
-        label="Описание"
-      />
-      {/* </Form>
-      </FormikProvider> */}
-    </Stack>
-  );
+interface ValidationShema {
+  [key: string]: (value: string) => string;
 }
 
+type ConfirmationMode = 'additing' | 'deleting' | 'editing';
+
+const StyledTooltip = styled(({ className, ...props }: TooltipProps) => (
+  <Tooltip
+    {...props}
+    classes={{ popper: className }}
+    placement="bottom-start"
+  />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: theme.palette.error.main,
+    color: theme.palette.error.contrastText,
+    marginLeft: '24px',
+    marginTop: '5px !important',
+    fontSize: '0.75rem',
+  },
+}));
+
+const CustomCellEditForm = (props: GridRenderEditCellParams) => {
+  const { colDef, value, id, field, error } = props;
+
+  const apiRef = useGridApiContext();
+
+  const handleCellOnChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    apiRef.current.setEditCellValue({ id, field, value });
+  }, [apiRef, field, id]);
+
+  return (
+    <StyledTooltip open={!!error} title={error} >
+      <div className={styles.cellContainer}>
+        <TextField
+          placeholder={`Введите ${colDef.headerName?.toLowerCase()}`}
+          value={value ?? ''}
+          onChange={handleCellOnChange}
+          fullWidth
+          error={!!error}
+        />
+      </div>
+    </StyledTooltip>
+  );
+};
+
 export function TaskTypes(props: TaskTypesProps) {
+  const apiRef = useGridApiRef();
   const { data: taskTypes = [], isFetching, isLoading } = useGetTaskTypesQuery();
   const [insertTaskType] = useAddTaskTypeMutation();
-  const [updateTaskType, { isLoading: updateTaskTypeIsLoading }] = useUpdateTaskTypeMutation();
-  const [deleteTaskType, { isLoading: deleteTaskTypeIsLoading }] = useDeleteTaskTypeMutation();
-  const [titleAndMethod, setTitleAndMethod] = useState<{title: string, method: () => void}>({ title: '', method: () => {} });
-  const handleSetTAM = (title: string, method: () => void) => {
-    setTitleAndMethod({ title, method });
-  };
+  const [updateTaskType] = useUpdateTaskTypeMutation();
+  const [deleteTaskType] = useDeleteTaskTypeMutation();
   const [confirmOpen, setConfirmOpen] = useState(false);
+  const [titleAndMethod, setTitleAndMethod] = useState<{
+    title: string,
+    mode: ConfirmationMode,
+    method: () => void
+      }>({
+        title: '', mode: 'additing', method: () => {}
+      });
+
+  const handleSetTitleAndMethod = (mode: ConfirmationMode, method: () => void) => {
+    const title = (() => {
+      switch (mode) {
+        case 'additing':
+          return 'Сохранение нового типа задач';
+        case 'editing':
+          return 'Редактирование типа задач';
+        case 'deleting':
+          return 'Удаление типа задач';
+        default:
+          return 'Сохранение нового типа задач';
+      }
+    })();
+    setTitleAndMethod({ title, mode, method });
+  };
 
   const handleSubmit = useCallback((taskType: ITaskType) => {
     if (taskType.ID > 0) {
@@ -142,103 +102,74 @@ export function TaskTypes(props: TaskTypesProps) {
     };
   }, []);
 
-  function EditToolbar() {
-    const handleClick = () => {
-      const id = 0;
-      apiRef.current.updateRows([{ ID: id, isNew: true }]);
-      apiRef.current.setRowIndex(id, 0);
-      apiRef.current.scrollToIndexes({
-        rowIndex: 0,
-      });
-      apiRef.current.setRowMode(id, 'edit');
-      // Wait for the grid to render with the new row
-      setTimeout(() => {
-        apiRef.current.scrollToIndexes({
-          rowIndex: 0,
-        });
-        apiRef.current.setCellFocus(id, 'name');
-      }, 150);
+  function RowMenuCell(props: GridRenderCellParams) {
+    const { id } = props;
+    const api = useGridApiContext();
+    const isInEditMode = api.current.getRowMode(id) === 'edit';
+
+    const handleEditClick = (event: MouseEvent<HTMLButtonElement>) => {
+      event.stopPropagation();
+      api.current.setRowMode(id, 'edit');
     };
 
-    return (
-      <GridToolbarContainer>
-        <div style={{ paddingRight: '20px', paddingTop: '6px', width: '100%', display: 'flex', justifyContent: 'flex-end' }}>
-          <Button
-            variant="contained"
-            disabled={isFetching}
-            onClick={handleClick}
-          >
-            Добавить
-          </Button>
-        </div>
-      </GridToolbarContainer>
-    );
-  }
-
-  function RowMenuCell(props: any) {
-    const { api, id } = props;
-    const isInEditMode = api.getRowMode(id) === 'edit';
-
-    const handleEditClick = (event: any) => {
+    const handleConfirmSave = (event: MouseEvent<HTMLButtonElement>) => {
       event.stopPropagation();
-      api.setRowMode(id, 'edit');
-    };
-
-    const handleConfirmSave = (event: any) => {
-      event.stopPropagation();
-      const row = api.getRow(id);
+      const row = api.current.getRow(id);
       if (row!.isNew) {
-        handleSetTAM('Сохранение нового типа задач', handleSaveClick);
+        handleSetTitleAndMethod('additing', handleSaveClick);
       } else {
-        handleSetTAM('Редактирование типа задач', handleSaveClick);
+        handleSetTitleAndMethod('editing', handleSaveClick);
       }
       setConfirmOpen(true);
     };
 
     const handleSaveClick = () => {
       setConfirmOpen(false);
-      api.commitRowChange(id);
-      api.setRowMode(id, 'view');
-      const row = api.getRow(id);
+      api.current.commitRowChange(id);
+      api.current.setRowMode(id, 'view');
+
+      const row = api.current.getRow(id);
       if (row!.isNew) delete row['ID'];
       handleSubmit(row);
     };
 
-    const handleConfirmDelete = (event: any) => {
+    const handleConfirmDelete = (event: MouseEvent<HTMLButtonElement>) => {
       event.stopPropagation();
-      handleSetTAM('Удаление типа задач', handleDeleteClick);
+      handleSetTitleAndMethod('deleting', handleDeleteClick);
       setConfirmOpen(true);
     };
 
     const handleDeleteClick = () => {
       setConfirmOpen(false);
-      deleteTaskType(id);
+      deleteTaskType(Number(id));
     };
 
     const handleCancelClick = () => {
-      api.setRowMode(id, 'view');
+      api.current.setRowMode(id, 'view');
 
-      const row = api.getRow(id);
+      const row = api.current.getRow(id);
       if (row!.isNew) {
-        api.updateRows([{ ID: id, _action: 'delete' }]);
+        api.current.updateRows([{ ID: id, _action: 'delete' }]);
       }
     };
 
     if (isInEditMode) {
-      return (<>
-        <GridActionsCellItem
-          icon={<SaveIcon />}
-          label="Save"
-          onClick={handleConfirmSave}
-        />
-        <GridActionsCellItem
-          icon={<CancelIcon />}
-          label="Cancel"
-          className="textPrimary"
-          onClick={handleCancelClick}
-          color="inherit"
-        />
-      </>);
+      const rowModel = apiRef.current.getEditRowsModel();
+      return (
+        <>
+          <GridActionsCellItem
+            icon={<SaveIcon />}
+            label="Save"
+            onClick={handleConfirmSave}
+            color="primary"
+            disabled={!rowModel[id]['NAME']?.value || !!rowModel[id]['NAME']?.error}
+          />
+          <GridActionsCellItem
+            icon={<CancelIcon />}
+            label="Cancel"
+            onClick={handleCancelClick}
+          />
+        </>);
     }
 
     return (
@@ -246,45 +177,85 @@ export function TaskTypes(props: TaskTypesProps) {
         <GridActionsCellItem
           icon={<EditIcon />}
           label="Edit"
-          className="textPrimary"
           onClick={handleEditClick}
-          color="inherit"
+          color="primary"
         />
         <GridActionsCellItem
           icon={<DeleteIcon />}
           label="Delete"
           onClick={handleConfirmDelete}
-          color="inherit"
+          color="primary"
         />
       </>
     );
   }
 
+  const validationShema: ValidationShema = {
+    NAME: (value) => {
+      if (value.length > 10) return 'Слишком длинное наименование';
+      return '';
+    }
+  };
+
+  const validationCell = (fieldName: string) => (params: GridPreProcessEditCellProps) => {
+    const errorMessage = validationShema[fieldName](params.props.value ?? '');
+    return { ...params.props };
+  };
+
+  const renderEditCell = (params: GridRenderEditCellParams) => <CustomCellEditForm {...params} />;
+
   const columns: GridColumns = [
-    { field: 'NAME', headerName: 'Наименование', editable: true, flex: 0.5, },
-    { field: 'DESCRIPTION', editable: true, headerName: 'Описание', flex: 1, },
+    {
+      field: 'NAME',
+      headerName: 'Наименование',
+      editable: true,
+      flex: 0.5,
+      renderEditCell,
+      // preProcessEditCellProps: validationCell('NAME'),
+    },
+    {
+      field: 'DESCRIPTION',
+      editable: true,
+      headerName: 'Описание',
+      flex: 1,
+      renderEditCell,
+    },
     {
       field: 'actions',
       type: 'actions',
       resizable: false,
-      renderCell: RowMenuCell
+      renderCell: RowMenuCell,
     }
   ];
 
-  const apiRef = useGridApiRef();
-
   const handleRowEditStart = (
     params: GridRowParams,
-    event: MuiEvent<React.SyntheticEvent>,
+    event: MuiEvent<SyntheticEvent>,
   ) => {
     event.defaultMuiPrevented = true;
   };
 
   const handleRowEditStop = (
     params: GridRowParams,
-    event: any,
+    event: MuiEvent,
   ) => {
     event.defaultMuiPrevented = true;
+  };
+
+  const handleCellKeyDown = (params: GridCellParams, event: MuiEvent<KeyboardEvent<HTMLElement>>) => {
+    if (event.key === 'Enter') {
+      event.defaultMuiPrevented = true;
+    }
+  };;
+
+  const handleAddSource = () => {
+    const id = 0;
+    apiRef.current.updateRows([{ ID: id, isNew: true }]);
+    apiRef.current.setRowIndex(id, 0);
+    apiRef.current.scrollToIndexes({
+      rowIndex: 0,
+    });
+    apiRef.current.setRowMode(id, 'edit');
   };
 
   const memoConfirmDialog = useMemo(() =>
@@ -292,12 +263,14 @@ export function TaskTypes(props: TaskTypesProps) {
       open={confirmOpen}
       title={titleAndMethod.title}
       text="Вы уверены, что хотите продолжить?"
+      dangerous={titleAndMethod.mode === 'deleting'}
       confirmClick={titleAndMethod.method}
       cancelClick={() => {
         setConfirmOpen(false);
       }}
     />
   , [confirmOpen, titleAndMethod]);
+
 
   return (
     <>
@@ -307,15 +280,21 @@ export function TaskTypes(props: TaskTypesProps) {
         className={styles.card}
       >
         <CardHeader title={<Typography variant="h3">Типы задач</Typography>} />
+        <Divider />
+        <CardToolbar>
+          <Button
+            className={styles.addButton}
+            variant="contained"
+            disabled={isFetching}
+            onClick={handleAddSource}
+          >
+            Добавить
+          </Button>
+        </CardToolbar>
         <CardContent
           className={styles.cardContent}
         >
           <StyledGrid
-            initialState={{
-              sorting: {
-                sortModel: [{ field: 'rating', sort: 'desc' }],
-              },
-            }}
             editMode="row"
             rows={taskTypes}
             columns={columns}
@@ -324,12 +303,7 @@ export function TaskTypes(props: TaskTypesProps) {
             apiRef={apiRef}
             onRowEditStart={handleRowEditStart}
             onRowEditStop={handleRowEditStop}
-            components={{
-              Toolbar: EditToolbar,
-            }}
-            componentsProps={{
-              toolbar: { apiRef },
-            }}
+            // onCellKeyDown={handleCellKeyDown}
             hideHeaderSeparator
             hideFooter
           />
