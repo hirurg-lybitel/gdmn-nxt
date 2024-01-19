862972ccb6dde289fbf8b79ef56165545b9e9ddd
hirurg-lybitel <yurashoihet@gmail.com>
2024-01-20 00:45:01 +0300

    actions test 3

--- apps/gdmn-nxt-web/src/app/const.ts
+++ apps/gdmn-nxt-web/src/app/const.ts
@@ -1,5 +1,6 @@
 import { config } from '@gdmn-nxt/config';
 
 // export const baseUrl = `https://${config.serverHost}${config.serverPort ? `:${config.serverPort}` : ''}/`;
+console.log('const', config.serverHost);
 export const baseUrl = `https://${config.serverHost}/`;
 export const baseUrlApi = `${baseUrl}api/v1/`;
