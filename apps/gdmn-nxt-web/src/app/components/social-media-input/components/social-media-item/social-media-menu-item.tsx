fb22b9147c5d793d2661e4646a7c80c29932a0d7
hirurg-lybitel <yurashoihet@gmail.com>
2024-02-14 15:06:00 +0300

    cleaning

--- apps/gdmn-nxt-web/src/app/components/social-media-input/components/social-media-item/social-media-menu-item.tsx
+++ apps/gdmn-nxt-web/src/app/components/social-media-input/components/social-media-item/social-media-menu-item.tsx
@@ -27,7 +27,7 @@ export function SocialMediaMenuItem(props: SocialMediaMenuItemProps) {
       data-testid={`option-${socialName}`}
     >
       <Styled.ListItemIcon>
-        <img style={{ width: '30px' }} src={socialMediaIcons[socialName]}/>
+        <img style={{ width: '25px' }} src={socialMediaIcons[socialName]}/>
       </Styled.ListItemIcon>
       <Styled.ListItemText>
         {socialName}
