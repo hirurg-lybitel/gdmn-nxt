150c360ff179a85ec6f564a38e868d7da8b04f80
hirurg-lybitel <yurashoihet@gmail.com>
2024-05-11 03:37:12 +0300

    Update mailingRouter.ts

--- apps/gdmn-nxt-server/src/app/routes/mailingRouter.ts
+++ apps/gdmn-nxt-server/src/app/routes/mailingRouter.ts
@@ -5,7 +5,8 @@ import { mailingController } from '@gdmn-nxt/controllers/mailing';
 const router = express.Router();
 router.get('/marketing/segments', segmentsController.getAll);
 
-router.get('/marketing/mailing', mailingController.getAll);
+router.get('/marketing/mailing', mailingController.findAll);
+router.get('/marketing/mailing/:id', mailingController.findOne);
 router.post('/marketing/mailing', mailingController.createMailing);
 router.put('/marketing/mailing/:id', mailingController.updateById);
 router.post('/marketing/mailing/launch/:id', mailingController.launchMailing);
