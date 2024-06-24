6a4a693a3780774e448d7b013df8ae99c3cfdeda
hirurg-lybitel <yurashoihet@gmail.com>
2024-06-24 20:09:58 +0300

    moved hooks to hook folder

--- apps/gdmn-nxt-web/src/app/features/common/getCookie.ts
+++
@@ -1,9 +0,0 @@
-export function getCookie(name: string) {
-  const cookie: {[key: string]: string} = {};
-  const parts = document.cookie.split(';');
-  parts.forEach(e => {
-    const [key, value] = e.split('=');
-    cookie[key.trim()] = value;
-  });
-  return cookie[name];
-}
