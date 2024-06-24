a755f347fbf13ffe4dfe50c2718b2ec378324bc3
hirurg-lybitel <yurashoihet@gmail.com>
2024-06-24 19:48:07 +0300

    refactor: deleted trash

--- apps/gdmn-nxt-web/src/app/customers/CustomerDetails/act-completion/columns.tsx
+++ apps/gdmn-nxt-web/src/app/customers/CustomerDetails/act-completion/columns.tsx
@@ -11,30 +11,30 @@ export const columns: Columns<IActCompletion> = {
     { field: 'NUMBER', headerName: 'Номер', minWidth: 150 },
     { field: 'DOCUMENTDATE', headerName: 'Дата', width: 100,
       type: 'date',
-      renderCell: ({ value }: any) => value.toLocaleString('default', { day: '2-digit', month: '2-digit', year: '2-digit' }),
+      renderCell: ({ value }) => value.toLocaleString('default', { day: '2-digit', month: '2-digit', year: '2-digit' }),
     },
     { field: 'USR$SUMNCU', headerName: 'Сумма', width: 100, align: 'right',
-      renderCell: ({ value }: any) => (Math.round(value * 100) / 100).toLocaleString(undefined, { minimumFractionDigits: 2 })
+      renderCell: ({ value }) => (Math.round(value * 100) / 100).toLocaleString(undefined, { minimumFractionDigits: 2 })
     },
     {
       field: 'JOBWORKNAME', headerName: 'Вид работ', flex: 1, minWidth: 200,
-      renderCell: ({ value }: any) => <Box style={{ width: '100%', whiteSpace: 'initial' }}>{value}</Box>
+      renderCell: ({ value }) => <Box style={{ width: '100%', whiteSpace: 'initial' }}>{value}</Box>
     }
   ],
   [ContractType.BG]: [
     { field: 'NUMBER', headerName: 'Номер', minWidth: 150 },
     { field: 'DOCUMENTDATE', headerName: 'Дата', width: 100,
       type: 'date',
-      renderCell: ({ value }: any) => value.toLocaleString('default', { day: '2-digit', month: '2-digit', year: '2-digit' }),
+      renderCell: ({ value }) => value.toLocaleString('default', { day: '2-digit', month: '2-digit', year: '2-digit' }),
     },
     { field: 'DEPT_NAME', headerName: 'Отдел', width: 100 },
     { field: 'JOB_NUMBER', headerName: 'Заказ', width: 100 },
     { field: 'USR$SUMNCU', headerName: 'Сумма', width: 100, align: 'right',
-      renderCell: ({ value }: any) => (Math.round(value * 100) / 100).toLocaleString(undefined, { minimumFractionDigits: 2 })
+      renderCell: ({ value }) => (Math.round(value * 100) / 100).toLocaleString(undefined, { minimumFractionDigits: 2 })
     },
     {
       field: 'JOBWORKNAME', headerName: 'Вид работ', flex: 1, minWidth: 200,
-      renderCell: ({ value }: any) => <Box style={{ width: '100%', whiteSpace: 'initial' }}>{value}</Box>
+      renderCell: ({ value }) => <Box style={{ width: '100%', whiteSpace: 'initial' }}>{value}</Box>
     }
   ]
 };
