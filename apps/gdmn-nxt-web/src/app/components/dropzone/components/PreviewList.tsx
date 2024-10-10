1e6449f77c6eec4a0aeded4ae8c5557251eb11a4
Sasha <94962537+sashazernin@users.noreply.github.com>
2024-10-10 14:13:43 +0300

    dropzone file name

--- apps/gdmn-nxt-web/src/app/components/dropzone/components/PreviewList.tsx
+++ apps/gdmn-nxt-web/src/app/components/dropzone/components/PreviewList.tsx
@@ -1,7 +1,7 @@
 import { isImage } from '../helpers';
 import { FileObject } from '../types';
 import styles from './preview.module.less';
-import { Box, Fab } from '@mui/material';
+import { Box, Fab, Tooltip } from '@mui/material';
 import AttachFileIcon from '@mui/icons-material/AttachFile';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { MouseEvent } from 'react';
@@ -83,21 +83,22 @@ export function PreviewList({
       className={styles['container']}
     >
       {files?.map((file, idx) => (
-        <Box
-          key={idx}
-          className={styles['imageContainer']}
-          onClick={handleDownload(file)}
-        >
-          {getPreviewIcon(file)}
-          <Fab
-            aria-label="Delete"
-            size="small"
-            className={styles['removeButton']}
-            onClick={handleRemove(idx)}
+        <Tooltip key={idx} title={file.file.name}>
+          <Box
+            className={styles['imageContainer']}
+            onClick={handleDownload(file)}
           >
-            <DeleteIcon />
-          </Fab>
-        </Box>
+            {getPreviewIcon(file)}
+            <Fab
+              aria-label="Delete"
+              size="small"
+              className={styles['removeButton']}
+              onClick={handleRemove(idx)}
+            >
+              <DeleteIcon />
+            </Fab>
+          </Box>
+        </Tooltip>
       ))}
     </Box>
   );
