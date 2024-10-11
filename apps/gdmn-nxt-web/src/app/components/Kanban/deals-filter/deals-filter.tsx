16986ddfe4e59a7b0a84f82948c017baabece0b5
Sasha <94962537+sashazernin@users.noreply.github.com>
2024-10-11 20:37:20 +0300

    fix: deal number field

--- apps/gdmn-nxt-web/src/app/components/Kanban/deals-filter/deals-filter.tsx
+++ apps/gdmn-nxt-web/src/app/components/Kanban/deals-filter/deals-filter.tsx
@@ -78,6 +78,11 @@ export function DealsFilter(props: DealsFilterProps) {
     return () => clearTimeout(sendRequestNumber);
   }, [requestNumber]);
 
+  const handleDealNumberChnage = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    if (isNaN(Number(e.target.value))) return;
+    setDealNumber(e.target.value);
+  };
+
   return (
     <CustomizedDialog
       open={open}
@@ -103,7 +108,7 @@ export function DealsFilter(props: DealsFilterProps) {
             <TextField
               label="Номер сделки"
               value={dealNumber || ''}
-              onChange={(e) => setDealNumber(e.target.value)}
+              onChange={handleDealNumberChnage}
             />
             <Autocomplete
               options={customers}
